f74b667621e9d903b52d0d1ab7834295bae5da6a
santiago Alarcon <santiagoalarcon2@hotmail.com>
2019-11-15 20:24:00 -0300

    de todo un poco

--- src/index.js
+++ src/index.js
@@ -1,55 +1,76 @@
 const express = require('express')
 const cors = require('cors')
-const bodyparse = require('body-parser')
-const app = express()
+const bodyparser = require('body-parser')
 
 const myUser = {
-	email: "santiagoalarcon2@hotmail.com",
-	password: "123456789"
+  email: "santiagoalarcon@gmail.com",
+  password: "123456"
 }
 
-app.use(bodyparse.json())
+const app =express()
+app.use(bodyparser.json())
 app.use(cors())
 
-app.get("/", (req, res) => {
-	res.status(200).send("server working")
+app.get("/", (req, res) =>{
+  console.log(req.body);
+  res.status(200).send("server working")
 })
 
 app.post("/login", (req, res) => {
+  if(!req.body.email) {
+    return res.status(400).send({
+      success: false,
+      message: "Email is required",
+    })
+  }
+  if(!req.body.password) {
+    return res.status(400).send({
+      success: false,
+      message: "Password is required",
+    })
+  }
 
-	if(!req.body.email)
-	{
-			return res.status(400).send({
-				success: false,
-				message: "Email is required"
-			})
-	}
+  //modficacion para mostrar si el error es el user o el password
+  //no aplicable en produccion
 
-	if(!req.body.password)
-	{
-			return res.status(400).send({
-				success: false,
-				message: "Password is required"
-			})
-	}
+      if( req.body.email !== myUser.email  )
+      {
+        return res.status(401).send ({
+          success: false,
+          message: "User not exist",
+        })
+      }
 
-	if(req.body.email !== myUser.email || req.body.password !== myUser.password)
-	{
-		return res.status(400).send({
-				success: false,
-				message: "User is not valid"
-		})
-	}
-	
-	return res.status(200).send({
-				success: true,
-				message: "User logged successfully",
-				user: myUser
-	})
+        if( req.body.password !== myUser.password    )
+        {
+        return res.status(401).send ({
+          success: false,
+          message: "wrong password  ",
+        })
+      }
 
+
+
+
+  /*if( req.body.email !== myUser.email  || req.body.password !== myUser.password  )
+  {
+    return res.status(401).send ({
+      success: false,
+      message: "User not exist",
+    })
+  }*/
+
+
+
+
+  return res.status(200).send({
+    success: true,
+    message: "User logged successfully",
+    user: myUser,
+  })
 })
 
 
-app.listen(4000, () => {
-	console.log("server working on localhost:4000")
+app.listen(4000, () =>{
+  console.log("Server working on localhost")
 })
